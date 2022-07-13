3b3a8c763c78003cf3f0a3ba17f60516fa2e7c5e
anthony-b1 <anthony78510@gmail.com>
2022-07-13 05:33:33 -0400

    Added More Info to Student Model
    Added:
    imageUrl
    email
    gpa

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -30,11 +30,15 @@ const seedDB = async () => {
 	const dummy_student = await Student.create({
 		firstname: "Joe",
       lastname: "Smith",
+	  email: "jsmith@gmail.com",
+	  gpa: 3.6,
 	});
 	// Create a new student for a campus
 	const dummy_student2 = await Student.create({
 		firstname: "Mary",
       lastname: "Johnson",
+	  email: "mjohnson@gmail.com",
+	  gpa: 4.0,
 	});
 
 	// Add students to campuses
